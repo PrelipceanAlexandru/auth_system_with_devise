c22235b988e082fb38c58c1bd86f36ce1ee4cb5a
Alex9508 <prelipceanalexg@gmail.com>
2019-02-23 22:44:11 +0200

    Controller endpoint return json

--- app/controllers/c_tests_controller.rb
+++ app/controllers/c_tests_controller.rb
@@ -2,6 +2,9 @@ class CTestsController < ApplicationController
   before_action :authenticate_user!
 
   def hello
+    
     @message = "Hello #{current_user.email}"
+
+    render json: { message: @message }
   end
 end
